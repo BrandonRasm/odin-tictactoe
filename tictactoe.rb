d89213b3a68391de0290f9859205b894c737bf3d
Brandon Rasmussen <BrandonRasm0412@gmail.com>
2022-02-08 12:38:14 -0800

    created a board with the initialize methon of the Board class
    created display_board method that prints out the board in command line

--- tictactoe.rb
+++ tictactoe.rb
@@ -3,6 +3,7 @@
 # General game state control. Turns and player info
 class TicTacToe
   def initialize
+    @board = Board.new
     @p1 = Player.new('X')
     @p2 = Player.new('O')
   end
@@ -25,7 +26,19 @@ end
 
 # Holds the state of the board and the logic to determine outcomes
 class Board
-  def initialize; end
+  def initialize
+    @board = Array.new(3) { Array.new(3, ' ') }
+    displayBoard
+  end
+
+  def display_board
+    counter = 0
+    @board.each do |row|
+      print counter.zero? ? '' : "-----\n"
+      puts "#{row[0]}|#{row[1]}|#{row[2]}"
+      counter += 1
+    end
+  end
 end
 
 x = TicTacToe.new
